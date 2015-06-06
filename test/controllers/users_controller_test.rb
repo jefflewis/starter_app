b7574da9c6fd3b8ff10cf1e1ec92eb0833b9b4bd
Jeff Lewis <lewisj88@mac.com>
2015-06-06 12:56:17 -0700

    Add password resets & email configuration

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,14 +44,14 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when not logged in" do
     assert_no_difference "User.count" do
       delete :destroy, id: @user
     end
     assert_redirected_to login_url
   end
-  
+
   test "should redirect destroy when logged in as a non-admin" do
     log_in_as(@other_user)
     assert_no_difference 'User.count' do
@@ -59,7 +59,7 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to root_url
   end
-  
+
   test "should not allow the admin attribute to be edited via the web" do
     log_in_as(@other_user)
     assert_not @other_user.admin?
